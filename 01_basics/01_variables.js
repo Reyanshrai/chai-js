81f5d2e5f0ba7699f983fede95d6d7eedefdcf65
Reyansh Rai <136559583+Reyanshrai@users.noreply.github.com>
2024-05-07 17:59:38 +0530

    initilaize of variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12333;
+let email = "reyanhs@gamil.com";
+var accountPassword = "1234";
+accountcity = "jaipur"; 
+
+// accountId = 2322;  constant not allowed to change
+email= "rey@gmailcom";
+accountPassword ="21212";
+accountcity= "delhi";
+
+
+/* 
+ prefer not use var because issue in block scope and functional scope
+
+*/
+
+
+console.table([accountId,email,accountPassword,accountcity]);
