f885f9bbab884ad49f17ee4debfb11e35adb44a4
Reyansh Rai <136559583+Reyanshrai@users.noreply.github.com>
2024-05-14 00:41:52 +0530

    add file

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,73 @@
+// let score = 33;
+// let name = "33abc";
+
+// console.log(typeof(name));
+
+// let valueInNumber = Number(name);
+
+// console.log(valueInNumber);
+// console.log( typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// // false => 0
+
+// let isloggedIn = 1;
+
+// let booleanISloggesIn = Boolean(isloggedIn)
+
+// console.log(booleanISloggesIn)
+
+
+// 1 => true;
+// 0 => false
+
+// "hitesh" => true
+// "" => false
+
+
+// let someNumber = 33;
+
+// let stringNumber = String(someNumber);
+
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+// ***************** Operations **
+
+// let value = 3;
+// let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2**3);
+// console.log(2%3);
+
+// let str1 = "hello";
+// let str2 = " reyansh";
+// let str3 = str1+str2;
+
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+console.log(true);
+console.log(+true);
+
+let num1 , num2 ,num3;
+
+num1=num2=num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
