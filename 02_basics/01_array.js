b04f90afcb3c13a952e93a0819cfb7dda8cb03a7
Reyansh Rai <136559583+Reyanshrai@users.noreply.github.com>
2024-05-23 12:56:59 +0530

    add array file

---
+++ 02_basics/01_array.js
@@ -0,0 +1,43 @@
+// array 
+
+const myArr = [0,1,2,3,4,5,7];
+
+const myHeors = ['shakiman','nagraj'];
+
+const myArr2 = new Array(1,2,3,4,5)
+ 
+console.log(myArr[4]);
+
+// Array Method
+
+
+// myArr.push(6) push is use add element in last index
+
+// myArr.pop() remove element last value of array
+
+// myArr.unshift(9) but unshift add value starting  index
+
+// myArr.shift()  shift remove starting element
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(1))
+
+// const newArr = myArr.join()
+
+// console.log(myArr)
+// console.log(newArr)
+// console.log(typeof newArr)
+
+// slice , splice
+
+// console.log("A", myArr);
+
+// const myn1 = myArr.slice(1,3)
+// console.log(myn1)
+
+console.log("B",myArr)
+
+//const myn2 = myArr.splice(1,3) change original array 
+console.log("c",myArr)
+console.log(myn2)
+
