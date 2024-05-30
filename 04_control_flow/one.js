801cebc8f7d016d4e29ad737a6522f8453fb3e2d
Reyansh Rai <136559583+Reyanshrai@users.noreply.github.com>
2024-05-30 22:19:27 +0530

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,13 @@
+// if(condition){}
+
+// let temp = 40
+// if( temp >=40){
+//     console.log("it is too hot")
+// }else{
+//     console.log('Its normal')
+// }
+
+const balance =1000;
+ // if(balance > 500) console.log('test'); its no good
+
+ 
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,30 @@
+const userEmail = []
+
+// if(userEmail){
+//     console.log('got user email')
+// }else{
+//     console.log("dont have user email")
+// }
+
+// falsy values  :- false , 0 , -0 ,BigInt 0n , "" , null , undefinid , NaN
+
+// truthy value :- '0' , 'false' , ' ', [], {}, function(){}
+
+
+// if(userEmail.length===0){
+//     console.log('Array is empty')
+// }
+
+// const emtyObj = {}
+
+// if(Object.keys(emtyObj).length===0){
+//     console.log('obj is empty ')
+// }
+
+
+// nullish coalescing operator (??) : null , undeifnied
+
+let bal1 ;
+bal1 = 5 ?? 10
+
+console.log(bal1)
